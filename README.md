7234ad4bfce410c2948891e3464f6500fc636c57
LeiYangGH <675686066@qq.com>
2022-01-26 22:46:52 +0800

    error for valid request

--- README.md
+++ README.md
@@ -24,3 +24,7 @@ response:
 ```bash
 2022-01-26 21:16:15.585  WARN 62072 --- [nio-8080-exec-2] .w.s.m.s.DefaultHandlerExceptionResolver : Resolved [org.springframework.http.converter.HttpMessageNotReadableException: JSON parse error: Unexpected character ('"' (code 34)): was expecting a colon to separate field name and value; nested exception is com.fasterxml.jackson.core.JsonParseException: Unexpected character ('"' (code 34)): was expecting a colon to separate field name and value<EOL> at [Source: (PushbackInputStream); line: 1, column: 25]]
 ```
+if log first:
+```bash
+2022-01-26 22:45:47.196 ERROR 84456 --- [nio-8080-exec-1] com.example.demo.GreetController         : Required request body is missing: public java.lang.String com.example.demo.GreetController.greet(com.example.demo.WelcomeMessage)
+```
--- src/main/java/com/example/demo/wrapper/MyFilter.java
+++ src/main/java/com/example/demo/wrapper/MyFilter.java
@@ -3,6 +3,8 @@ package com.example.demo.wrapper;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Order;
+import org.springframework.core.Ordered;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.*;
@@ -10,13 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 //https://stackoverflow.com/questions/7318632/java-lang-illegalstateexception-getreader-has-already-been-called-for-this-re
+//@Order(Ordered.LOWEST_PRECEDENCE)
 public class MyFilter implements Filter {
     protected static final Logger log = LogManager.getLogger();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
-
+//can work for both valid and both invalid request, but log after response
 //    @Override
 //    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 //        final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
@@ -26,13 +29,15 @@ public class MyFilter implements Filter {
 //        log.info(requestBody);
 //    }
 
-    @Override
+    //log before response, but only work for invalid request
+    //for valid request: Required request body is missing
+//    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper(httpServletRequest);
 //        String requestBody = IOUtils.toString(cachedRequest.getContentAsByteArray(), cachedRequest.getCharacterEncoding());
-        String requestBody = IOUtils.toString(cachedRequest.getInputStream(), cachedRequest.getCharacterEncoding());
-//        String requestBody = IOUtils.toString(cachedRequest.getReader());
+//        String requestBody = IOUtils.toString(cachedRequest.getInputStream(), cachedRequest.getCharacterEncoding());
+        String requestBody = IOUtils.toString(cachedRequest.getReader());
         log.info(requestBody);
         chain.doFilter(cachedRequest, response);
     }
